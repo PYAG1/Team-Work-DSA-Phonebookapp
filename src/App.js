0d87cbba325a03db0b1a55de0e49702753cef24b
PYAG1 <96929032+PYAG1@users.noreply.github.com>
2022-11-23 22:56:30 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Main from "./other";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App(){
+const [formse,setforms]= React.useState({
+name:"",
+number:""
+});
+
+
+function changeV(event){
+setforms(prev => {
+  return {
+    ...prev, [event.target.name] : event.target.value
+  }
+})
+};
+
+
+const [number, setnumber]= React.useState([]);
+
+
+console.log(number)
+
+
+function AddBtn(){
+
+
+
+  const Name=document.getElementById("name");
+  Name.value= ''
+  setnumber(prev =>{
+    if(formse.number.length != 10){
+      console.log("Wrong Address")
+      return[...prev]
+    }
+    else{
+    
+
+
+      return [ ...prev , formse]
+      
+  
+     
+    }
+
+  
+
+  })
+
+
+
 }
 
-export default App;
+
+const value=number.map(function(item){
+return <Main 
+{...item}
+/>
+})
+
+
+
+
+
+return(
+  <div>
+    <form>
+      <label htmlFor="name">Name</label>
+      <br></br>
+      <input type="text" id="name" name="name" value={formse.name} placeholder="Enter your name" onChange={changeV} />
+
+<br></br>
+      <label htmlFor="number">number</label>
+      <br></br>
+      <input type="number" id="number" name="number" placeholder="Enter your number" value={formse.number} onChange={changeV}/>
+      <p></p>
+      
+      
+    </form>
+<button onClick={AddBtn}>Add </button>
+{value}
+
+
+  </div>
+)
+
+
+}
---
+++ src/other.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+export default function Main(props){
+    return(
+<div className="show">
+    <h3>{props.name}</h3>
+    <p>{props.number}</p>
+</div>
+
+
+
+
+    )
+}
